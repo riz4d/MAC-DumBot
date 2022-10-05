559cfa46ecec0dedefe89802e2fcc7a4c2fba397
Mohamed Rizad <rizadvillan@gmail.com>
2022-10-05 14:25:59 +0530

    Confgs:(

---
+++ Config.py
@@ -0,0 +1,8 @@
+API_KEY='<PUT API-KEY HERE>' #replace this with API KEY
+API_ID = '<PUT API-ID HERE>' #replace this with API ID
+API_HASH = '<PUT API-HASH HERE>' #replace this with API HASH
+BOT_TOKEN = '<PUT BOT-TOKEN HERE>' #replace this with BOT-TOKEN
+
+# CONSTANT
+
+credsurl='https://api.macaddress.io/v1/credits?apiKey='
